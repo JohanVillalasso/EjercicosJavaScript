bd0c1fc1f9fb65f34f8217b78957aa55b77041b0
JohanVillalasso <113487194+JohanVillalasso@users.noreply.github.com>
2022-11-08 22:14:57 -0500

    primer ejercicio de JS

---
+++ js/main.js
@@ -0,0 +1,24 @@
+alert("Hola mundo, estoy aprendiendo Java Script");
+alert("uno");
+alert("dos");
+alert("tres");
+alert("cuatro");
+alert("cinco");
+
+//Este es un comentario de linea
+
+/*
+Este es un comentario de bloque
+*/
+
+// alt + shift + A = comando para comentario de bloque, mas rapido
+
+
+//console.log("Hola mundo, esto es un mensaje de consola");
+
+//tipos de datos
+console.log(123456789) //numeros
+console.log("Johan") // caracter
+console.log(true) //(false) booleanos
+
+document.write("Hola! Estoy escribiendo en la pagina web");
